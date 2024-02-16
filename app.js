f5845286ae95dd311e1532843523a01dd7178036
m.azzam <m.azzam.azis@gmail.com>
2024-02-16 11:29:25 +0700

    added ui to the rps game
    its now a button based game

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ function getComputerChoice () {
   }
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection) {
   playerSelection = playerSelection.toLowerCase()
-  computerSelection = computerSelection.toLowerCase()
+  computerSelection = getComputerChoice().toLowerCase()
 
   const WIN = "WIN";
   const LOSE = "LOSE";
@@ -41,25 +41,51 @@ function playRound (playerSelection, computerSelection) {
   
 }
 
-function playgame() {
-  let userScore = 0;
-  let computerScore = 0;
+const buttons = document.querySelector('.container').childNodes;
+let playerScore = 0;
+let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, paper or scissor?");
-    const computerSelection = getComputerChoice();  
-    
-    let res = playRound(playerSelection, computerSelection);
-    if (res === "WON") {
-      userScore++;
-      console.log("user win");
-    } else if (res === "LOSE") {
-      computerScore++;
-      console.log("computer win");
-    } else {
-      console.log("its a draw");
+const user = document.querySelector('#user');
+const computer = document.querySelector('#computer');
+
+const playerRes = document.createElement('h3'); 
+const computerRes = document.createElement('h3');
+
+user.appendChild(playerRes);
+computer.appendChild(computerRes);
+
+const section = document.querySelector('section');
+const message = document.createElement('h2'); 
+
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    playerRes.parentNode.style.backgroundColor = 'white';
+    computerRes.parentNode.style.backgroundColor = 'white';
+    message.textContent = '';
+    if (playRound(button.id) === "WIN") {
+      playerScore ++;
+    } else if (playRound(button.id) === "LOSE") {
+      computerScore ++;
     }
-  }
-  console.log(`final score is: user ${userScore} - ${computerScore} computer`);
-}
-playgame();
+
+    playerRes.textContent = playerScore;
+    computerRes.textContent = computerScore;
+
+    if (computerScore === 5 || playerScore === 5) {
+      if (computerScore < playerScore) {
+        message.textContent = 'you won';
+        playerRes.parentNode.style.backgroundColor = 'rgb(0, 204, 0)';
+      } else {
+        message.textContent = 'you lost';
+        computerRes.parentNode.style.backgroundColor = 'rgb(240, 0, 0)';
+      }
+      section.appendChild(message);
+      
+      playerScore = 0;
+      computerScore = 0;
+    }
+
+  });
+});
+
